c618612daef595b50bca58946248cca7728a3cb3
tiagopereira2000 <tgpaa@iscte-iul.pt>
2023-05-09 22:07:14 +0100

    Criação dos modelos (needing review  & testing)

--- WebApp/models.py
+++ WebApp/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.checks import Tags
 from django.db import models
 
 # Create your models here.
@@ -7,7 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     stock = models.IntegerField(default=0)
     price = models.FloatField(default=0.0)
-    # tags = models
+    # tags = models.ManyToManyRel(Tags)
 
 class Client(models.Model):
     user= models.OneToOneField(User, on_delete=models.CASCADE)
@@ -15,11 +16,12 @@ class Client(models.Model):
     is_active = models.BooleanField(default=True)
 
 class Cart(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE())
-    #product_list
-    #last_change
-    #is_active
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, on_delete=models.CASCADE)
+    last_change = models.DateTimeField()
 
-#class Order(models.Model):
-class Tag(models.Model):
-    name = models.CharField(max_length=20)
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    cart = models.ForeignKey(Cart, on_delete=models.RESTRICT)
+    payment_method = models.CharField()
+    charged = models.FloatField()
