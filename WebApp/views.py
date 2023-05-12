4444b67b3649884ccbba804d4392780a1dba7581
tiagopereira2000 <tgpaa@iscte-iul.pt>
2023-05-12 21:02:37 +0100

    stash

--- WebApp/views.py
+++ WebApp/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from .models import Product, Cart, Order, Client
 from .forms import *
 
+
 @login_required(login_url='login')
 def home(request):
     products = Product.objects.all()
@@ -22,30 +23,35 @@ def loginview(request):
         return redirect('home')
     return render(request, 'login.html', {'form': form})
 
+
 def logoutview(request):
     logout(request)
     return redirect('login')
+
 
 @login_required(login_url='login')
 def cart(request):
     # in_cart = Cart.user.cart_set
     return render(request, 'cart.html')
 
+
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     context = {"product": product}
     return render(request, 'product.html', context)
 
+
 def edit_product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    if request.method=='POST':
+    if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
             return redirect('edit_product', product_id=product_id)
     else:
         form = ProductForm(instance=product)
-    return render(request, 'editProduct.html', {"form":form, "product":product})
+    return render(request, 'editProduct.html', {"form": form, "product": product})
+
 
 def cart(request):
     # Obtém o cliente associado ao usuário atual
@@ -59,6 +65,7 @@ def cart(request):
 
     # Renderiza o template de exibição do carrinho com os produtos
     return render(request, 'cart.html', {'products': products})
+
 
 def removefromcart(request, product_id):
     cart = request.user.client.cart
@@ -75,6 +82,7 @@ def removefromcart(request, product_id):
         cart.save()
 
     return redirect('cart')
+
 
 def add2cart(request, product_id):
     cart = request.user.client.cart
