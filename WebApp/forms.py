d7c29400e0e2e6ac3ba5d1498bb95d9597f3085b
Gonçalo Lopes <gfcls@iscte-iul.pt>
2023-05-13 18:05:16 +0100

    REGISTER SEM CSS

--- WebApp/forms.py
+++ WebApp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import *
 
 
@@ -12,3 +14,10 @@ class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['name', 'profile_image']
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
